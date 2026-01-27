716f0ab5f8b8337890b8e80ddf3f04da20dade2e
Parth Patel <parthspatel.nj@gmail.com>
2026-01-26 22:42:51 -0800

    init

--- target/debug/deps/redline_core-45d7b06eb3f7e469.d
+++
@@ -1,5 +0,0 @@
-/Users/parthpatel/Documents/projects/glyph/target/debug/deps/redline_core-45d7b06eb3f7e469.d: crates/core/src/lib.rs
-
-/Users/parthpatel/Documents/projects/glyph/target/debug/deps/libredline_core-45d7b06eb3f7e469.rmeta: crates/core/src/lib.rs
-
-crates/core/src/lib.rs:
--- target/debug/deps/redline_core-b2869fc7dba89a6b.d
+++
@@ -1,5 +0,0 @@
-/Users/parthpatel/Documents/projects/glyph/target/debug/deps/redline_core-b2869fc7dba89a6b.d: crates/core/src/lib.rs
-
-/Users/parthpatel/Documents/projects/glyph/target/debug/deps/libredline_core-b2869fc7dba89a6b.rmeta: crates/core/src/lib.rs
-
-crates/core/src/lib.rs:
